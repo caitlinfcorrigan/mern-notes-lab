027102d125e4ddd5179118199401d5277181feac
caitlincorrigan <caitlinfcorrigan@gmail.com>
2023-11-17 08:50:54 -0800

    updates NavBar and pages directory

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -3,9 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import React from 'react';
 import AuthPage from '../AuthPage/AuthPage';
+import NotesPage from '../NotesPage/NotesPage';
 import NavBar from '../../components/NavBar/NavBar';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import './App.css';
 
 export default function App() {
@@ -17,8 +16,7 @@ export default function App() {
       <>
         <NavBar user={ user } setUser={ setUser }/>
         <Routes>
-          <Route path="/orders/new" element={ <NewOrderPage /> } />
-          <Route path="/orders" element={ <OrderHistoryPage /> } />
+          <Route path="/" element={ < NotesPage /> } />
         </Routes>
       </>
       :
